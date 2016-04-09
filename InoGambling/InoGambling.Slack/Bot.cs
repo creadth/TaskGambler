56c9a955fed7a82a1634a93d95b1b3e8fbcad7ea
Tatiana Shamsutdinova <tashainverse@gmail.com>
2016-04-09 00:51:05 +0000

    FirstSlackBotMessage

---
+++ InoGambling/InoGambling.Slack/Bot.cs
@@ -0,0 +1,25 @@
+﻿using SlackAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InoGambling.Slack
+{
+    class Bot
+    {
+        static void Main()
+        {            
+            SlackSocketClient client = new SlackSocketClient("xoxb-33255991907-GIsd17eRb8bMgLuVMrm4Uv8H");
+            client.Connect((connected) => {
+                //This is called once the client has emitted the RTM start command               
+
+            }, () => {
+                //This is called once the RTM client has connected to the end point
+                client.PostMessage(null, "#general", "Party Hard", "Bot", null, false, null, false,
+                   null, null, true);
+            });
+        }
+    }
+}
